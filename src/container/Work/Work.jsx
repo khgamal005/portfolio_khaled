125c978fdc4cd8337656eb4218020e1b1c6ff3c3
kgamal005 <khgamal005@gmail.com>
2026-06-09 01:01:36 +0300

    Show project card overlay on mobile (hover: none)

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -66,11 +66,7 @@ setTimeout(() => {
             >
               <img src={urlFor(work.imgUrl)} alt={work.name} />
 
-              <motion.div
-                whileHover={{ opacity: [0, 1] }}
-                transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
-                className="app__work-hover app__flex"
-              >
+              <div className="app__work-hover app__flex">
                 <a href={work.projectLink} target="_blank" rel="noreferrer">
 
                   <motion.div
@@ -92,7 +88,7 @@ setTimeout(() => {
                     <AiFillGithub />
                   </motion.div>
                 </a>
-              </motion.div>
+              </div>
             </div>
 
             <div className="app__work-content app__flex">
